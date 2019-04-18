dbee67f1d99b1e03c22a8b9c5eec4040892b97b8
Alex Rakowsky <rakoalex3@gmail.com>
2019-04-18 10:35:37 -0400

    your name added

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Sen Zhang" );
+        System.out.println( "Alex Rakowsky" );
 	System.out.println(" First Last");
 
     }
